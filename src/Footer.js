17ff62c2ca15681754aa67e8231238e3a62f7570
Vihar Revankar <viharrevankar100@gmail.com>
2021-01-28 19:23:57 +0530

    update footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 function Footer() {
     return(
         <div className="footer">
-            <a href="#">Vihar Revankar &copy;2021</a>
+            <a href="https://imviharevankar.github.io/portfolio/" target="_blank">Vihar Revankar &copy;2021</a>
         </div>
     )
 }
